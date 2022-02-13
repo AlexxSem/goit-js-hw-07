3a99bb4231da7b2983ddc773583d1c16f830aacf
Oleksandr Semenchuk <semenchyk@ukr.net>
2022-02-13 15:51:34 +0200

    ДЗ 7_done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+const galleryRef = document.querySelector('.gallery');
 
-console.log(galleryItems);
+//Функция отрисовки галереи
+function galleryRender(arrayOfImages) {
+    const imagesArr = [];
+    galleryItems.map(({ preview, original, description }) => {
+        imagesArr.push(`<div class="gallery__item">
+                            <a class="gallery__link" href="large-image.jpg">
+                                <img
+                                class="gallery__image"
+                                src="${preview}"
+                                data-source="${original}"
+                                alt="${description}"
+                                />
+                            </a>
+                        </div>`);
+    });
+    galleryRef.insertAdjacentHTML('beforeend', imagesArr.join(''));
+}
+galleryRender(galleryItems);
+
+galleryRef.addEventListener('click', openOriginalImage); //Слушатель клика на галереи div
+function openOriginalImage(event) {
+    event.preventDefault(); // отменяем дефолтное поведение ссылок
+
+        if (event.target.classList.contains('gallery__image')) {
+        const instance = basicLightbox.create(
+            // открываем модальное окно
+            `<img src="${event.target.dataset.source}" width="800" height="600">`,
+            {
+                onClose: instance =>
+                    document.removeEventListener('keydown', onEscClick), // снимаем слушатель клавиатуры при закрытие модалки
+            },
+        );
+        instance.show();
+
+        document.addEventListener('keydown', onEscClick); // добавляем слушатель клавиатуры
+        function onEscClick(event) {
+            // проверяем как кнопка нажата
+            if (event.code === 'Escape') {
+                // если ESC то закрываем модалку
+                instance.close();
+            }
+        }
+    } else {
+        alert('Мы в Вас верим, что вы сможете попасть по картинке ))))))');
+    }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+//Функция отрисовки gallery
+function galleryRender(arrayOfImages) {
+    const imagesArr = [];
+    galleryItems.map(({ preview, original, description }) => {
+        imagesArr.push(`<a class="gallery__item" href="${original}">
+                            <img
+                                class="gallery__image"
+                                src="${preview}" 
+                                alt="${description}" 
+                            />
+                        </a>`);
+    });
+    galleryRef.insertAdjacentHTML('beforeend', imagesArr.join(''));
+}
+galleryRender(galleryItems);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 300,
+});
