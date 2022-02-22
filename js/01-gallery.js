ee30a5f5f93097077f3ae0255aec9618813918e1
Oleksandr Semenchuk <semenchyk@ukr.net>
2022-02-22 23:41:08 +0200

    правка

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,59 +1,48 @@
 import { galleryItems } from './gallery-items.js';
-const galleryRef = document.querySelector('.gallery');
+// Change code below this line
 
-//Функция отрисовки галереи
-function galleryRender(arrayOfImages) {
-    const imagesArr = galleryItems.map;
-    galleryItems.map(({ preview, original, description }) => {
-        return (`<div class="gallery__item">
-                            <a class="gallery__link" href="large-image.jpg">
-                                <img
-                                class="gallery__image"
-                                src="${preview}"
-                                data-source="${original}"
-                                alt="${description}"
-                                />
-                            </a>
-                        </div>`);
-    });
-    galleryRef.insertAdjacentHTML('beforeend', imagesArr.join(''));
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItems(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup)
+galleryContainer.addEventListener('click', onGalleryClick);
+function createGalleryItems(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+    </a>
+    </div>
+`
+    })
+.join('')
 }
-galleryRender(galleryItems);
-
-galleryRef.addEventListener('click', openOriginalImage); //Слушатель клика на галереи div
-function openOriginalImage(event) {
-    event.preventDefault(); // отменяем дефолтное поведение ссылок
-
-        if (event.target.classList.contains('gallery__image')) {
-        const instance = basicLightbox.create(
-                        `<img src="${event.target.dataset.source}" width="800" height="600">`,
-            {
-                onShow: instance => {
-                    window.addEventListener('keydown', onEscClick);
-                },
-            },
-            {
-                onClose: instance => {
-                    window.removeEventListener('keydown', onEscClick);
-                },
-                },
-        );
-            
-            function onImgClick(event) {
-                event.preventDefault();
-                if (event.target.nodeName !== 'IMG') {
-                    return;
-                }
-                instance.show();
-            }
-        
-        function onEscClick(event) {
-            if (event.code === 'Escape') {
-                instance.close();
-                return;
-            }
-        }
-    } else {
-        alert('Мы в Вас верим, что вы сможете попасть по картинке ))))))');
+function onGalleryClick(e) {
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') {
+    return;
     }
+    modalShow(e.target.dataset.source);    
 }
+    function modalShow(src) {
+        const instance = basicLightbox.create(
+        `<img src="${src}" width="800" height="600">`,
+            {
+            onShow: () => {
+                window.addEventListener("keydown", onEscClick);
+            },
+            onClose: () => window.removeEventListener("keydown",  onEscClick),
+            }
+        );
+function onEscClick(e) {
+    if (e.code === 'Escape') {
+    instance.close();
+    }  
+}
+instance.show()
+};
